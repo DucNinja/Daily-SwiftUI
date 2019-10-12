735e75d00efbceb6f9c990682d2f2ec77f3f1980
Duc <ducito@msn.com>
2019-10-12 11:13:11 +0700

    Setup project

---
+++ Issue 1_Hello SwiftUI/Hello SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello SwiftUI
+//
+//  Created by Duc Nguyen on 10/12/19.
+//  Copyright © 2019 Duc Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
